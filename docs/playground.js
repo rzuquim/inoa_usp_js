52eeba9c837f4a2e4a8717c0140929bcf3a639d1
Rafael Zuquim <rzuquim@inoa.com.br>
2020-10-30 23:06:24 -0300

    playgroud aula 4

--- docs/playground.js
+++ docs/playground.js
@@ -65,31 +65,91 @@
 // let message = person.age >= legalVotingAge[country] ? "Pode votar" : "Não vota";
 // console.log(message);
 
-function criadorDeAlunos(nome, numeroUSP) {
-  let aluno = {
-    nome: nome,
-    numeroUSP: numeroUSP,
-  };
+// function criadorDeAlunos(nome, numeroUSP) {
+//   let aluno = {
+//     nome: nome,
+//     numeroUSP: numeroUSP,
+//   };
 
-  aluno.estudar = (materia) => {
-    for (let hours = 0; hours <= 2; hours++) {
-      console.log(`Estou estudando ${materia} por ${hours}...`);
-    }
-    console.log(`Já deu por hoje`);
-  };
-  return aluno;
-}
+//   aluno.estudar = (materia) => {
+//     for (let hours = 0; hours <= 2; hours++) {
+//       console.log(`Estou estudando ${materia} por ${hours}...`);
+//     }
+//     console.log(`Já deu por hoje`);
+//   };
+//   return aluno;
+// }
 
-let rafael = criadorDeAlunos("Rafael", 1234);
-rafael.estudar("matematica");
+// let rafael = criadorDeAlunos("Rafael", 1234);
+// rafael.estudar("matematica");
 
-let veronica = criadorDeAlunos("Veronica", 4321);
+// let veronica = criadorDeAlunos("Veronica", 4321);
 
-let alunos = [rafael, veronica];
+// let alunos = [rafael, veronica];
 
-let imprimeNome = function (aluno) {
-  console.log(`aluno.nome ${aluno}`);
-  return aluno.nome;
-};
+// let imprimeNome = function (aluno) {
+//   console.log(`aluno.nome ${aluno}`);
+//   return aluno.nome;
+// };
 
-alunos.forEach(imprimeNome);
+// alunos.forEach(imprimeNome);
+
+// let myCounter = (function () {
+//   let counter = 0;
+
+//   return function count() {
+//     return ++counter;
+//   };
+// })();
+
+// let nicePeople = [{ name: "Lucas" }, { name: "Matheus" }];
+// nicePeople.push({ name: "Vitor" });
+
+// function isCreative(color, creativeColors = ["red", "blue", "yellow"]) {
+//   return creativeColors.includes(color);
+// }
+
+// let person = {
+//   first_name: "Jonathan",
+//   last_name: "Hsu",
+//   mediu_handle: "@jhsu98",
+// };
+
+// let colors = ["red", "blue", "yellow"];
+
+// function range(start, end) {
+//   let response = [];
+//   for (let i = start; i < end; i++) {
+//     response.push(i);
+//   }
+//   return response;
+// }
+
+// for (let i of range(0, 100)) {
+//   console.log(i);
+// }
+
+// const values = ["a", "b", "c", "d", "e", "c"];
+
+// /**
+//  * Método que todos os elementos iguais a element
+//  * @param {Array} array
+//  * @param {any} element
+//  */
+// function remove(array, element) {
+//   let index;
+//   let removed = [];
+//   while ((index = array.indexOf(element)) > 0) {
+//     removed = removed.concat(array.splice(index, 1));
+//   }
+//   return removed;
+// }
+
+// const part = remove(values, "c");
+// console.log(part);
+// console.log(values);
+
+const letters = ["a", "b", "c", "d", "e", "c"];
+const numbers = [0, 1, 2, 3, 4, 5];
+
+let values = [...letters, ...numbers];
