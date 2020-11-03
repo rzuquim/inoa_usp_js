c5b569bd3e0842362e8eeded4c553a8d4ed7ad54
Lucas Perez <lucastperez@gmail.com>
2020-11-02 23:35:12 -0300

    Outra solução para o ex_02_04

--- exercicios/aula-02/ex_02_04_solution.js
+++ exercicios/aula-02/ex_02_04_solution.js
@@ -24,38 +24,55 @@ var studentGrades = [
   ["Miguel", 6.8],
 ];
 
-// seu código aqui
-var classGrade = 0;
+var classGrade = 'F'
+var media = 0;
 for(let i = 0 ; i < studentGrades.length ; i++){
-  classGrade += studentGrades[i][1]
+  media += studentGrades[i][1]
 }
-classGrade = classGrade/studentGrades.length;
+media = media/studentGrades.length;
 
-if(classGrade < 6)
+// 1
+let notas = [6.0, 6.2, 6.6, 6.9, 7.2, 7.6, 7.9, 8.2, 8.6, 8.9, 9.2, 9.6]
+let conceitos = ['F','D-','D','D+','C-','C','C+','B-','B','B+','A-','A','A+']
+
+for(let i = conceitos.length - 1; i >= 0; i--) {
+    if(media >= notas[i]) {
+      classGrade = conceitos[i+1];
+      break;
+    }
+}
+
+console.log(classGrade === "B-");
+
+// 2
+/* Poderíamos ver um por um, esse
+   seria o método da "força bruta" */
+
+if(media < 6)
   classGrade = 'F';
-else if(classGrade <= 6.2)
+else if(media <= 6.2)
   classGrade = 'D-';
-else if(classGrade <= 6.6)
+else if(media <= 6.6)
   classGrade = 'D';
-else if(classGrade <= 6.9)
+else if(media <= 6.9)
   classGrade = 'D+';
-else if(classGrade <= 7.2)
+else if(media <= 7.2)
   classGrade = 'C-';
-else if(classGrade <= 7.6)
+else if(media <= 7.6)
   classGrade = 'C';
-else if(classGrade <= 7.9)
+else if(media <= 7.9)
   classGrade = 'C+';
-else if(classGrade <= 8.2)
+else if(media <= 8.2)
   classGrade = 'B-';
-else if(classGrade <= 8.6)
+else if(media <= 8.6)
   classGrade = 'B';
-else if(classGrade <= 8.9)
+else if(media <= 8.9)
   classGrade = 'B+';
-else if(classGrade <= 9.2)
+else if(media <= 9.2)
   classGrade = 'A-';
-else if(classGrade <= 9.6)
+else if(media <= 9.6)
   classGrade = 'A';
-else if(classGrade <= 10)
+else if(media <= 10)
   classGrade = 'A+';
 
 console.log(classGrade === "B-");
